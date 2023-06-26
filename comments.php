94ca8e50a301d2c9846c1cf32eb772dd76d39906
arash-khd <126689940+arash-khd@users.noreply.github.com>
2023-06-27 02:38:56 +0330

    Update comments.php

--- comments.php
+++ comments.php
@@ -1,7 +1,11 @@
  <div class="user-comments">
   <div class="comments">
   <p>نظرات کاربران <i class="fa fa-comments" aria-hidden="true"></i></p>
-  <?php comment_form(); ?>
+  <?php comment_form(); 
+/* add comment form , this code make default comment form then we go in a functions.php and customize
+  as what we want , there are all a lot af ways to customize comment form and we didnt the best way so i dont put my codes for customize comment form in this repository 
+  */
+?>
   </div>  
   <?php
 
@@ -9,7 +13,6 @@ if(have_comments()){
     $awplc = array(
         'walker'            => null,
         'max_depth'         => 2,
-        'style'             => 'div',
         'callback'          => 'mytheme_comment',
         'end-callback'      => null,
         'type'              => 'comment',
@@ -25,37 +28,34 @@ if(have_comments()){
     );
 
     function mytheme_comment($comment, $awplc, $depth) {
-        $GLOBALS['comment'] = $comment;
-        extract($awplc, EXTR_SKIP);
-            $tag = 'div';
-            $add_below = 'comment';
-        
+
         ?>
-        <<?php echo $tag ?> <?php comment_class( [empty( $awplc['has_children'] ) ? '' : 'parent' ,"user-comment" ]) ?> id="comment-<?php comment_ID() ?>">
 
-         <?php printf( __( '<p class="comment-author">%s<span class="says">%s1</span></p>' ), get_comment_author() , get_comment_date('Y/m/d')); ?>
-
-
-        <?php if ( $comment->comment_approved == '0' ) : ?>
-            <span class="comment-awaiting-moderation" style="color: #3daa59"><?php _e( 'دیدگاه شما پس از بررسی و تایید ، نمایش داده خواهد شد' ); ?></span>
+<div <?php comment_class( [empty( $awplc['has_children'] ) ? '' : 'parent' ,'user-comment' ]) ?> id="comment-<?php comment_ID(); ?>" >
+    <p class="comment-author"><?php echo get_comment_author(); ?><span><?php echo get_comment_date('Y/m/d'); ?></span></p>
+    <?php if ( $comment->comment_approved == '0' ) : ?>
+     <span class="comment-awaiting-moderation" style="color: #3daa59"><?php _e( 'دیدگاه شما پس از بررسی و تایید ، نمایش داده خواهد شد' ); ?></span>
         <?php endif; ?>
+    <div class="comment-type"> 
+    <p>    
+   <?php echo get_comment_text(); ?>
+</p>
+    <div class="post-details">
+ <div class="reply-comment">
+ <?php
+  comment_reply_link( array_merge( $awplc, array('depth' => $depth, 'max_depth' => $awplc['max_depth'] ) ) );
+   ?>
+ </div>
 
-        <div class="comment-type"> 
-        <?php comment_text();?>
+ </div>
 
-            <div class="post-details">
-            <div class="reply-comment">
-            <?php comment_reply_link( array_merge( $awplc, array( 'add_below' => $add_below, 'depth' => $depth, 'max_depth' => $awplc['max_depth'] ) ) ); ?>
-            </div>
-            
-            <div class="comment_like_dislike">
+</div>
 
-            </div>
-           
-            </div>
-     
-        
-        </div>
+  </div>
+
+
+
+
 
 
 
@@ -74,22 +74,7 @@ if(have_comments()){
 
   ?>
 
-  
-    <div class="user-comment">
-    <p class="comment-author">آرش<span>1402/12/07</span></p>
-    <div class="comment-type"> 
-    <p>    
-    عالی بود در آینده یک آدم موفق خواهی شد </p>
-    <div class="post-details">
- <div class="reply-comment">
-پاسخ دهید
- </div>
 
- </div>
-
-</div>
-
-  </div>
 
 
 
